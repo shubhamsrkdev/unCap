43378aff71af520e3b50b1af897fee7190c5cddf
uday krishna g <uday krishna g>
2017-04-24 20:13:04 +0530

    added about feature
    integrated settings with connected screen

--- settings.py
+++ settings.py
@@ -3,10 +3,27 @@ from kivy.uix.screenmanager import Screen, SlideTransition
 from kivy.config import Config
 from kivy.core.window import Window
 import login
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from login import Status
 class Settings(Screen):        
+    def __init__(self,**kwargs):
+        super(Screen,self).__init__(**kwargs)
+        #self.prev=''
 
+    def setprev_(self,prev):
+        self.prev=prev
+        
+                
     def back(self):
-        Window.size = (340, 250)
+        if self.prev=='login':
+            Window.size = (340, 250)
+        if self.prev=='connected':
+            Window.size = (340, 150)
         self.manager.transition = SlideTransition(direction="down")
-        self.manager.current = 'login'
+        self.manager.current = self.prev
+
+    def about(self):
+        popup = Popup(title='Made by Uday.', content=Label(text='click outside the box to dismiss',font_size='12sp'),auto_dismiss=True,size_hint=(None, None), size=(200, 100),title_size='20sp',title_align='center')
+        popup.open()
+        
